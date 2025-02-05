65d85da583c0cd88c3946878d749cf39acd92bb7
Cristian <ramboy725@gmail.com>
2025-02-05 14:07:02 -0400

    update script.js

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,3 +1,48 @@
+// SCRIPT
+
+const supabase = createClient('https://tu-proyecto.supabase.co', 'tu-anon-key');
+
+
+// Función para guardar usuario y contraseña
+async function CrearCuenta(username, password) {
+    const {
+        data,
+        error
+    } = await supabase.from('Cuentas').insert([{
+            Usuario: username, Contraseña: password
+        }]);
+
+    if (error) {
+        console.error('Error al guardar las credenciales:', error);
+    } else {
+        console.log('Credenciales guardadas exitosamente:', data);
+    }
+}
+
+
+
+
+async function IniciarSeccion(username, password) {
+    const {
+        data,
+        error
+    } = await supabase.from('Cuentas').select('*').eq('Usuario', username).eq('Contraseña', password);
+
+    if (error) {
+        console.error('Error al verificar las credenciales:', error.message);
+        return false;
+    } else if (data.length > 0) {
+        console.log('Credenciales correctas:', data);
+        return true;
+    } else {
+        console.log('Credenciales incorrectas');
+        return false;
+    }
+
+}
+
+
+
 
 /* Escucha evento submit del form */
 document.getElementById('loginForm').addEventListener('submit', function(e) {
@@ -8,14 +53,12 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    /* Valida credenciales */
-    if (username === 'bella' && password === 'teamo') {
-        /* Muestra mensaje de éxito */
-        alert('Inicio de sesión exitoso');
-        /* Redirecciona a área privada */
-        window.location.href = 'seccion-privada.html';
-    } else {
-        /* Muestra error */
-        alert('Nombre de usuario o contraseña incorrectos');
+    IniciarSeccion(username, password).then(isValid => {
+        if (isValid) {
+            alert('Inicio de sesión exitoso');
+            window.location.href = 'seccion-privada.html';
+        } else {
+            alert('Nombre de usuario o contraseña incorrectos');
+        }
     }
-});
+    });
