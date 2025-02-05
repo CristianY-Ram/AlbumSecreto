bf8bf033ed452dc2b9570302780b3d19099a7310
Cristian <ramboy725@gmail.com>
2025-02-05 18:44:25 -0400

    Delete resources directory

--- resources/scripts/supabase-config.js
+++
@@ -1,9 +0,0 @@
-
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = process.env.Url_Supabase
-const supabaseKey = process.env.key_Supabase;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
