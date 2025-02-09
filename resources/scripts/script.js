656921735c1950ee69fbe81c20473f548b968992
Cristian <ramboy725@gmail.com>
2025-02-09 11:47:42 -0400

    update script.js

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -15,6 +15,36 @@ document.querySelectorAll('.changeForm').forEach(button => {
     });
 });
 
+document.getElementById('buttonCrearCuenta').addEventListener('click', function(e) {
+    const email = document.getElementById('correo').value;
+    const password = document.getElementById('createclave').value;
+
+    // Validar longitud del email
+    if (email.length < 5 || email.length > 50) {
+        alert('El correo debe tener entre 5 y 50 caracteres.');
+        return;
+    }
+
+    // Validar formato del email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Por favor, ingresa un correo electrónico válido.');
+        return;
+    }
+
+    // Validar longitud de la contraseña
+    if (password.length < 6 || password.length > 20) {
+        alert('La contraseña debe tener entre 6 y 20 caracteres.');
+        return;
+    }
+
+    // Enviar datos al servidor
+    enviarDatosAlServidor(email, password);
+});
+
+
+
+
 document.getElementById('Haks').addEventListener('click', function(e) {
 
     const username = document.getElementById('username').value;
