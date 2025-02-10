1a5140fa647e5603162e02af746d465e793205f0
Cristian <ramboy725@gmail.com>
2025-02-10 11:53:15 -0400

    update script.js

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -53,25 +53,18 @@ document.getElementById('buttonCrearCuenta').addEventListener('click', function(
     const email = document.getElementById('correo').value;
     const password = document.getElementById('createclave').value;
 
-    // Validar longitud del email
     if (email.length < 5 || email.length > 50) {
         alert('El correo debe tener entre 5 y 50 caracteres.');
         return;
     }
-
-    // Validar formato del email
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
         alert('Por favor, ingresa un correo electrónico válido.');
         return;
     }
-
-    // Validar longitud de la contraseña
-    if (password.length < 6 || password.length > 20) {
-        alert('La contraseña debe tener entre 6 y 20 caracteres.');
+    if (password.length < 6 || password.length > 25) {
+        alert('La contraseña debe tener entre 6 y 25 caracteres.');
         return;
     }
-
-    // Enviar datos al servidor
     Enviar(email, password);
 });
