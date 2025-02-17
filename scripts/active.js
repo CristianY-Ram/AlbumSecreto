2a4518b48aedfbb11ab5cf2425bdbc47a3dd5447
CristianY-Ram <ramboy725@gmail.com>
2025-02-17 14:30:41 -0400

    Menú desplegable oprimía el contenido (Solucionado)

--- scripts/active.js
+++ scripts/active.js
@@ -1,11 +1,7 @@
-// scripts/active.js
-
 function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("sidebar").classList.add("open");
 }
 
 function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("sidebar").classList.remove("open");
 }
