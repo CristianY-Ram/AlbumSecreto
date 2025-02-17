b990533b9b5aa2b3a5d341d6ce7e53f376549f6c
CristianY-Ram <ramboy725@gmail.com>
2025-02-17 19:22:43 -0400

    Formatos del Texto Código

--- scripts/active.js
+++ scripts/active.js
@@ -1,8 +1,8 @@
 function moverNav() {
-    var menu = document.getElementById("Menu");
-    if (menu.classList.contains("abrir")) {
-        menu.classList.remove("abrir");
-    } else {
-        menu.classList.add("abrir");
-    }
+  var menu = document.getElementById("Menu");
+  if (menu.classList.contains("abrir")) {
+    menu.classList.remove("abrir");
+  } else {
+    menu.classList.add("abrir");
+  }
 }
